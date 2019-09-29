1f7ced20d9840a4180c6061e690356aa623ac77f
github-cyf <cuiyufeng1234@gmail.com>
2019-09-29 21:18:59 +0800

    新增configserver和zuul

---
+++ zuul/src/main/java/com/cyf/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.cyf.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableZuulServer
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
